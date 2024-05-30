96a350d79829412f9de92f36f36bc9f0f90f23dc
sharmaritik67 <ritiksharma67732@gmail.com>
2024-05-30 12:45:08 +0530

    Add files via upload

---
+++ Abstraction/src/Interface/Bike.java
@@ -0,0 +1,14 @@
+package Interface;
+
+public class Bike implements print{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("running is safely");
+	}
+	public static void main(String[] args) {
+		Bike b=new Bike();
+		b.run();
+	}
+
+}
